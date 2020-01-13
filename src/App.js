eb36612921d554cde6eb0f1327ca932fa03f7060
moerekh <simony@gmail.com>
2020-01-13 14:17:10 -0800

    Added topic dropdown menu

--- src/App.js
+++ src/App.js
@@ -2,7 +2,63 @@ import React from 'react';
 import class_catalog from './class_catalog.js';
 
 class StudentInterface extends React.Component {
+  constructor(props) {
+    let default_value = false;
+
+    super(props)
+    this.state = {
+      subject: default_value,
+      topic: default_value,
+      time: default_value
+    }
+
+    this.setSubjectOption = this.setSubjectOption.bind(this)
+    this.setTopicOption = this.setTopicOption.bind(this)
+  }
+
+  setSubjectOption = (event) => {
+    this.setState({
+      subject: event.target.value,
+      topic: this.default_value,
+      time: this.default_value
+    })
+  }
+
+  setTopicOption = (event) => {
+    this.setState({topic: event.target.value})
+  }
+
+  
+
   render() {
+
+    let topicOptions;
+    const subjectOptions = class_catalog.map((subject) => (
+      <OptionComponent 
+        key={subject.name}
+        value={subject.name}
+        label={subject.name}
+      ></OptionComponent>
+    ))
+
+    if (this.state.subject) {
+        for(let subject in class_catalog) {
+          let s = class_catalog[subject];
+         
+          if (s.name === this.state.subject) {
+            let topics = s.topics;
+            topicOptions = topics.map(topic => (
+              <OptionComponent 
+                key={topic.name}
+                value={topic.name}
+                label={topic.name}
+              ></OptionComponent>
+            ))
+          }
+          
+        }
+    }
+
     return (
       <div className="container">
         {/* Introductory text */}
@@ -10,7 +66,54 @@ class StudentInterface extends React.Component {
 
 
         {/* Form */}
-        <MyForm></MyForm>
+        <div className="container">
+          <form>
+            <TextInput fieldName="username" fieldLabel="Username"></TextInput>
+            <TextInput fieldName="email" fieldLabel="Email"></TextInput>
+
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <label>
+                    Subject:
+                    <select 
+                      className="form-control"
+                      value={this.state.subject}
+                      name="subject" 
+                      label="Subject:"
+                      onChange={this.setSubjectOption}
+                    >
+                      <option placeholder="Make a selection">Make a selection</option>
+                      {subjectOptions}
+                    </select>
+                  </label>
+                </div>
+              </div>
+            </div>
+            
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <label>
+                    Topic:
+                    <select 
+                      className="form-control"
+                      value={this.state.topic}
+                      name="topic" 
+                      label="Topic:"
+                      onChange={this.setTopicOption}
+                    >
+                      <option placeholder="Make a selection">Make a selection</option>
+                      {topicOptions}
+                    </select>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <AddClass></AddClass>
+          </form>
+        </div>
 
 
         {/* Table */}
@@ -23,13 +126,13 @@ class Introduction extends React.Component {
 
   render() {
       return(
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h1>Simon Younesi's Programming Challenge</h1>
-              </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Simon Younesi's Programming Challenge</h1>
             </div>
           </div>
+        </div>
       );
   }
 }
@@ -49,43 +152,11 @@ class TextInput extends React.Component {
   }
 }
 
-class SelectInput extends React.Component {
-  render() {
-    const options = class_catalog.map((subject) => (
-      <option value={subject.name}>{subject.name}</option>
-    ))
-
-    return(
-      <div className="row">
-        <div className="col">
-          <div className="form-group">
-            <label>
-              {this.props.label}
-              <select name="{this.props.name}" className="form-control">
-                <option selected disabled value="">Make a selection</option>
-                {options}
-              </select>
-            </label>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-class UsernameInput extends TextInput {
+class OptionComponent extends React.Component {
   render() {
     return(
-    <TextInput fieldName="username" fieldLabel="Username"></TextInput>
-    );
-  }
-}
-
-class EmailInput extends React.Component {
-  render() {
-    return(
-    <TextInput fieldName="email" fieldLabel="Email"></TextInput>
-    );
+      <option value={this.props.value}>{this.props.label}</option>
+    )
   }
 }
 
@@ -97,31 +168,11 @@ class AddClass extends React.Component {
   }
 }
 
-
-class MyForm extends React.Component {
-  render() {
-    return(
-      <div className="container">
-        <form>
-          <UsernameInput></UsernameInput>
-          <EmailInput></EmailInput>
-          <SelectInput 
-            name="subject" 
-            label="Subject:"
-          >
-          </SelectInput>
-          <AddClass></AddClass>
-        </form>
-      </div>
-    );
-  }
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
       </header>
       <main>
         <StudentInterface></StudentInterface>
